52a2a4b752071812fde513bd3865bcb129bc3311
full-stack-js-dev <baltidanger@live.fr>
2021-07-14 14:18:43 +0100

    update

--- middleware/workingHours.js
+++ middleware/workingHours.js
@@ -2,17 +2,14 @@ const date = new Date();
 const day = date.getUTCDay();
 const hours = date.getUTCHours();
 
-
-
 const isWorking = (req, res, next) => {
+  if ((day != 6 && day != 7)  && ( hours >=9 ||  17 <= hours ) ) {
+    next();
     
-  ((day != 6 && day != 7) && ( 17 <= hours >=9) )? 
-
-    next() :
-    
-      res.status(401).sendFile(__dirname + "/Closed.html");
-    
-  };
-  
-  // export the function
-  module.exports = isWorking;
+  } else {
+    res.status(401).sendFile(__dirname + "/Closed.html");
+  }
+};
+console.log(hours);
+// export the function
+module.exports = isWorking;
